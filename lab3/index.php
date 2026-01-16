3e841393f0566ae748f9152e4ed42eb406f996fb
Dmitrii229 <dimadantonov@gmail.com>
2026-01-16 12:54:34 +0300

    Lab3 commit

--- lab3/index.php
+++ lab3/index.php
@@ -1,3 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STAR</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="nikitka" id=""> 
+    <button
+         type="submit"
+        class="btn btn-primary">
+         GO TO NEXT PAGE
+    </button>
+
+    </form>
+</body>
+</html>
 <?php
-echo "hello world";
-?>
+if  (isset($_GET ['nikitka'])){
+    //var_dump($_GET ['nikitka']);
+
+    $unite=explode(' ',$_GET['nikitka']);
+    UnFunc($unite);
+
+    echo implode(' ',$unite);
+    
+    }
+    function UnFunc(&$unite){
+    for($i = 0; $i < count($unite); $i++){
+        if(($i % 2)>0){
+       $unite[$i] =  strtoupper($unite[$i]);
+        }
+    }
+        }
